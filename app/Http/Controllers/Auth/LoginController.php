1e18e447c54a44af7d9479da2b7f19adc506bd42
BagasMuharom <bagashidayat45@gmail.com>
2018-01-11 16:28:59 +0700

    Mengubah LoginController

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,38 +2,109 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Voting\Http\Controllers\Controller;
+use App\User;
+use App\Mahasiswa;
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
-
-    use AuthenticatesUsers;
 
     /**
-     * Where to redirect users after login.
+     * Melakukan proses login untuk seluruh user, kecuali
+     * mahasiswa
      *
-     * @var string
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
      */
-    protected $redirectTo = '/home';
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    public function login(Request $request)
     {
-        $this->middleware('guest')->except('logout');
+        $this->validate($request, [
+            'id' => 'required|numeric',
+            'password' => 'required'
+        ]);
+
+        $request->remember = (is_null($request->remember)) ? false : $request->remember;
+        if (Auth::attempt(['id' => $request->id, 'password' => $request->password], $request->remember)){
+            if (Auth::user()->isRoot()){
+                return redirect()->route('root.dashboard');
+            }
+            if (Auth::user()->isAdmin()){
+                return redirect()->route('admin.dashboard');
+            }
+            if (Auth::user()->isDosen() || Auth::user()->isWD1()){
+                return redirect()->route('dosen.dashboard');
+            }
+            if (Auth::user()->isKetuaKPU()){
+                return redirect()->route('kakpu.dashboard');
+            }
+            if (Auth::user()->isPanitia()){
+                return redirect()->route('panitia.dashboard');
+            }
+        }
+
+        return back()->with('message', 'NIP/NIM atau password salah!');
     }
+
+    /**
+     * Melakukan proses login untuk mahasiswa
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function loginMahasiswa(Request $request)
+    {
+        $this->validate($request, [
+            'id' => 'required|numeric',
+            'password' => 'required'
+        ]);
+
+        if ($request->password == Carbon::today()->toDateString()){
+            try {
+                $mahasiswa = Mahasiswa::findOrFail($request->id);
+            }
+            catch(ModelNotFoundException $err) {
+                return back()->with('message', 'NIM Salah');
+            }
+
+            if(!$mahasiswa->bisa_memilih)
+                return back()->with('message', 'Akun anda belum diaktivasi !');
+
+            Auth::guard('mhs')->login($mahasiswa);
+            return redirect()->route('mahasiswa.halaman.voting');
+        } else {
+            return back()->with('message', 'Password salah!');
+        }
+    }
+
+    /**
+     * Proses logout untuk seluruh user dan mahasiswa
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function logout(Request $request)
+    {
+        if(Auth::guard('web')->check()) {
+            Auth::logout();
+            return redirect()->route('admin.login.form');
+        }
+        else if(Auth::guard('mhs')->check()) {
+            Auth::guard('mhs')->logout();
+            return redirect()->route('mahasiswa.login');
+        }
+    }
+
+    public function form()
+    {
+        if (Auth::check()){
+            return back();
+        }
+        return view('admin.login');
+    }
+
 }
