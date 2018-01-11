df8d9670a922f6f7715c3abacb4474dde4ce9f56
m1ku100 <ilham.puji100@gmail.com>
2018-01-11 16:40:34 +0700

    route + Controller Ketua KPU

--- app/Http/Controllers/KakpuController.php
+++ app/Http/Controllers/KakpuController.php
@@ -6,5 +6,20 @@ use Illuminate\Http\Request;
 
 class KakpuController extends Controller
 {
-    //
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * Menampilkan halaman dashboard Ketua KPU
+     * nama routes : kakpu.dashboard
+     *
+     * @return \Illuminate\View\View
+     */
+    public function index()
+    {
+        return view('admin.kakpu.dashboard');
+    }
+
 }
