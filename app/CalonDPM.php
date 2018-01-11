a99da02967cdc197a3017311fbe369159063052b
Rafy Aulia Akbar <rafy683@gmail.com>
2018-01-11 17:17:33 +0700

    update model CalonDPM

--- app/CalonDPM.php
+++ app/CalonDPM.php
@@ -6,5 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class CalonDPM extends Model
 {
-    //
+    protected $table = 'calon_dpm';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'anggota_id', 'dir', 'visi', 'misi'
+    ];
+
+    /**
+     * mengambil data mahasiswa yang memilih
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function getPemilih()
+    {
+        return $this->belongsToMany('App\Mahasiswa','pemilihan_dpm','mahasiswa_id', 'calon_dpm_id')->withTimestamps();
+    }
+
+    /**
+     * mengambil data anggota
+     * @return Model|null|static
+     */
+    public function getAnggota()
+    {
+        return $this->belongsTo('App\Mahasiswa','ketua_id')->first();
+    }
 }
