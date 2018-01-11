4dbc1ddaabae268e4d9ce80937c228f18d13149b
Rafy Aulia Akbar <rafy683@gmail.com>
2018-01-11 15:52:59 +0700

    migrasi calon_hmj

--- database/migrations/2018_01_11_145712_calon_hmj.php
+++ database/migrations/2018_01_11_145712_calon_hmj.php
@@ -13,7 +13,24 @@ class CalonHmj extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('calon_hmj', function (Blueprint $table){
+            $table->increments('id');
+            $table->string('ketua_id');
+            $table->foreign('ketua_id')
+                ->references('id')
+                ->on('mahasiswa')
+                ->onUpdate('CASCADE')
+                ->onDelete('CASCADE');
+            $table->string('wakil_id');
+            $table->foreign('wakil_id')
+                ->references('id')
+                ->on('mahasiswa')
+                ->onUpdate('CASCADE')
+                ->onDelete('CASCADE');
+            $table->text('dir');
+            $table->text('visi');
+            $table->text('misi');
+        });
     }
 
     /**
