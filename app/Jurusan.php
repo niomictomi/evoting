da5676b8491e4e0509e28c5b372b637186f4340b
Rafy Aulia Akbar <rafy683@gmail.com>
2018-01-11 16:37:33 +0700

    model Jurusan

--- app/Jurusan.php
+++ app/Jurusan.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Jurusan extends Model
 {
-    //
+    protected $table = 'jurusan';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'nama'
+    ];
+
+    /**
+     * Mendapatkan, memasukkan dan menghapus data prodi
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function prodi()
+    {
+        return $this->hasMany('App\Prodi', 'jurusan_id');
+    }
 }
