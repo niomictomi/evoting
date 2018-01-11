c7d08a10d9270cfac5412979fc6e031dddb75df7
m1ku100 <ilham.puji100@gmail.com>
2018-01-11 16:51:35 +0700

    route + Controller Panitia

--- app/Http/Controllers/PanitiaController.php
+++ app/Http/Controllers/PanitiaController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class PanitiaController extends Controller
 {
-    //
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * Menampilkan halaman dashboard panitia
+     * nama routes : panitia.dashboard
+     *
+     * @return \Illuminate\View\View
+     */
+    public function index()
+    {
+        return view('admin.panitia.dashboard');
+    }
 }
--- routes/web/panitia.php
+++ routes/web/panitia.php
@@ -8,21 +8,9 @@
 
 //------------------------------Panitia--------------------------------//
 
-Route::group(['namespace' => 'Page\Panitia', 'prefix' => 'panitia'], function() {
+Route::group(['prefix' => 'panitia'], function() {
 
-    Route::get('dashboard', 'HmjController@index')->name('panitia.dashboard');
-
-    Route::get('paslon', 'HmjController@paslon')->name('panitia.paslon');
-
-    Route::get('form', 'HmjController@form')->name('panitia.form');
-
-    Route::post('form/add', 'HmjController@formsave')->name('panitia.form.save');
-
-    Route::get('resepsionis', 'HmjController@resepsionis')->name('panitia.resepsionis');
-
-    Route::get('resepsionis/search', 'HmjController@carimhs');
-
-    Route::post('resepsionis/{id}/update', 'HmjController@updatestatus');
+    Route::get('dashboard', 'PanitiaController@index')->name('panitia.dashboard');
 
 });
 
