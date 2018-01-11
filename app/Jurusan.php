1479e90d3cf0921a7ee5bab802bae0318dae7d4c
Rafy Aulia Akbar <rafy683@gmail.com>
2018-01-11 22:52:27 +0700

    penambahan method getMahasiswa pada model Jurusan

--- app/Jurusan.php
+++ app/Jurusan.php
@@ -22,4 +22,20 @@ class Jurusan extends Model
     {
         return $this->hasMany('App\Prodi', 'jurusan_id');
     }
+
+    /**
+     * mendapatkan data mahasiswa dari jurusan tertentu
+     * @return mixed
+     */
+    public function getMahasiswa()
+    {
+        $id_mhs = Array();
+        foreach ($this->prodi()->get() as $prodi){
+            $id_mhs = array_merge($id_mhs, array_flatten($prodi->mahasiswa()->get()->map(function ($mhs){
+                return collect($mhs->toArray())->only(['id'])->all();
+            })));
+        }
+
+        return Mahasiswa::whereIn('id', $id_mhs);
+    }
 }
