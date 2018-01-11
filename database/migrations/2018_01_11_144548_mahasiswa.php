9607add1ea8899e291d3cbfc5d60d2641b125b72
Rafy Aulia Akbar <rafy683@gmail.com>
2018-01-11 15:48:34 +0700

    migrasi mahasiswa

--- database/migrations/2018_01_11_144548_mahasiswa.php
+++ database/migrations/2018_01_11_144548_mahasiswa.php
@@ -13,7 +13,22 @@ class Mahasiswa extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('mahasiswa', function (Blueprint $table){
+            $table->string('id')
+                ->unique()
+                ->primary();
+            $table->integer('prodi_id');
+            $table->foreign('prodi_id')
+                ->references('id')
+                ->on('prodi')
+                ->onUpdate('CASCADE')
+                ->onDelete('SET NULL');
+            $table->string('nama');
+            $table->boolean('login')->default(false);
+            $table->boolean('hmj')->default(false);
+            $table->boolean('dpm')->default(false);
+            $table->boolean('bem')->default(false);
+        });
     }
 
     /**
