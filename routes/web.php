708e5076e744e37c7742f3c7463c4bcdf7e952cf
BagasMuharom <bagashidayat45@gmail.com>
2018-01-11 16:41:04 +0700

    Menambah route baru untuk mahasiswa login

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,27 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route public untuk login mahasiswa
+
+Route::group(['namespace' => 'Page', 'prefix' => 'mahasiswa'], function() {
+
+    Route::get('login', [
+        'uses' => 'MahasiswaController@login',
+        'as' => 'mahasiswa.login'
+    ]);
+
+});
+
+Route::prefix('mahasiswa')->group(function() {
+
+    Route::namespace('Auth')->group(function() {
+
+        Route::post('login', [
+            'uses' => 'LoginController@loginMahasiswa',
+            'as' => 'mahasiswa.login'
+        ]);
+
+    });
+
+});
