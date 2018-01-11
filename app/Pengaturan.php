bea7b9d64ec3e351ee0ee8658108aa831a0bef64
Rafy Aulia Akbar <rafy683@gmail.com>
2018-01-11 17:29:49 +0700

    model pengaturan

--- app/Pengaturan.php
+++ app/Pengaturan.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pengaturan extends Model
 {
-    //
+    protected $table = 'pengaturan';
+
+    protected $keyType = 'string';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'key', 'value'
+    ];
 }
