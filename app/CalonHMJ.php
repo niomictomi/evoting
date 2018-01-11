c2b36267258481e9020f8ed981adb84a59cfa0d4
Rafy Aulia Akbar <rafy683@gmail.com>
2018-01-11 17:12:30 +0700

    update model CalonHMJ

--- app/CalonHMJ.php
+++ app/CalonHMJ.php
@@ -6,5 +6,38 @@ use Illuminate\Database\Eloquent\Model;
 
 class CalonHMJ extends Model
 {
-    //
+    protected $table = 'calon_hmj';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'ketua_id', 'wakil_id', 'dir', 'visi', 'misi'
+    ];
+
+    /**
+     * mengambil data mahasiswa yang memilih
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function getMahasiswa()
+    {
+        return $this->belongsToMany('App\Mahasiswa','pemilihan_hmj','mahasiswa_id', 'calon_hmj_id')->withTimestamps();
+    }
+
+    /**
+     * mengambil data ketua
+     * @return Model|null|static
+     */
+    public function getKetua()
+    {
+        return $this->belongsTo('App\Mahasiswa','ketua_id')->first();
+    }
+
+    /**
+     * mengambil data wakil
+     * @return Model|null|static
+     */
+    public function getWakil()
+    {
+        return $this->belongsTo('App\Mahasiswa','wakil_id')->first();
+    }
 }
