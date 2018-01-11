1aaba5a4680bdcd6552263f4a2504812bc296cec
Rafy Aulia Akbar <rafy683@gmail.com>
2018-01-11 15:55:02 +0700

    migrasi calon bem

--- database/migrations/2018_01_11_145741_calon_bem.php
+++ database/migrations/2018_01_11_145741_calon_bem.php
@@ -13,7 +13,24 @@ class CalonBem extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('calon_bem', function (Blueprint $table){
+            $table->increments('id');
+            $table->string('ketua_id');
+            $table->foreign('ketua_id')
+                ->references('id')
+                ->on('mahasiswa')
+                ->onUpdate('CASCADE')
+                ->onDelete('CASCADE');
+            $table->string('wakil_id');
+            $table->foreign('wakil_id')
+                ->references('id')
+                ->on('mahasiswa')
+                ->onUpdate('CASCADE')
+                ->onDelete('CASCADE');
+            $table->text('dir');
+            $table->text('visi');
+            $table->text('misi');
+        });
     }
 
     /**
