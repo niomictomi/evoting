c38ecacacf36ffc73fb74461a27ff3e0c88520ad
Rafy Aulia Akbar <rafy683@gmail.com>
2018-01-11 17:34:07 +0700

    JurusanProdiSeeder

--- database/seeds/JurusanProdiSeeder.php
+++ database/seeds/JurusanProdiSeeder.php
@@ -1,9 +1,31 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Jurusan;
+use App\Prodi;
 
 class JurusanProdiSeeder extends Seeder
 {
+    const EKONOMI = [
+        'Pendidikan Ekonomi' => [
+            'S1 Pendidikan Ekonomi',
+            'S1 Pendidikan Administrasi Perkantoran',
+            'S1 Pendidikan Akuntansi',
+            'S1 Pendidikan Tata Niaga',
+        ],
+        'Manajemen' => [
+            'S1 Manajemen',
+        ],
+        'Akutansi' => [
+            'S1 Akuntansi',
+            'D3 Akuntansi'
+        ],
+        'Ilmu Ekonomi' => [
+            'S1 Ekonomi Islam',
+            'S1 Ilmu Ekonomi'
+        ]
+    ];
+
     /**
      * Run the database seeds.
      *
@@ -11,6 +33,16 @@ class JurusanProdiSeeder extends Seeder
      */
     public function run()
     {
-        //
+        foreach (static::EKONOMI as $jurusan => $daftarProdi){
+            $id = Jurusan::create([
+                'nama' => $jurusan
+            ])->id;
+            foreach ($daftarProdi as $prodi){
+                Prodi::create([
+                    'nama' => $prodi,
+                    'jurusan_id' => $id
+                ]);
+            }
+        }
     }
 }
