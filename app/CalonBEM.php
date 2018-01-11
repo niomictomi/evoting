01b62d2abbd06ec80d7bd2897986bb8abbbe3a85
Rafy Aulia Akbar <rafy683@gmail.com>
2018-01-11 17:14:03 +0700

    update model CalonBEM

--- app/CalonBEM.php
+++ app/CalonBEM.php
@@ -6,5 +6,38 @@ use Illuminate\Database\Eloquent\Model;
 
 class CalonBEM extends Model
 {
-    //
+    protected $table = 'calon_bem';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'ketua_id', 'wakil_id', 'dir', 'visi', 'misi'
+    ];
+
+    /**
+     * mengambil data mahasiswa yang memilih
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function getMahasiswa()
+    {
+        return $this->belongsToMany('App\Mahasiswa','pemilihan_bem','mahasiswa_id', 'calon_bem_id')->withTimestamps();
+    }
+
+    /**
+     * mengambil data ketua
+     * @return Model|null|static
+     */
+    public function getKetua()
+    {
+        return $this->belongsTo('App\Mahasiswa','ketua_id')->first();
+    }
+
+    /**
+     * mengambil data wakil
+     * @return Model|null|static
+     */
+    public function getWakil()
+    {
+        return $this->belongsTo('App\Mahasiswa','wakil_id')->first();
+    }
 }
