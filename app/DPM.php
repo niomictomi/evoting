03772d2f39adc605d6365be61b14fa3982e0850e
Rafy Aulia Akbar <rafy683@gmail.com>
2018-01-11 15:09:10 +0700

    penambahan file model

---
+++ app/DPM.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DPM extends Model
+{
+    //
+}
---
+++ app/HMJ.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class HMJ extends Model
+{
+    //
+}
