0a8acabe30c922d0185a3e9c3e8613b00aca8348
Rafy Aulia Akbar <rafy683@gmail.com>
2018-01-11 18:00:50 +0700

    update UserSeeder

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Factory;
+use App\Support\Role;
+use App\User;
 
 class UserSeeder extends Seeder
 {
@@ -11,6 +14,31 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $faker = Factory::create();
+        $id_panitia = 543210;
+        $id_others = 543220;
+
+        foreach (Role::ALL as $role){
+            if ($role == Role::PANITIA){
+                foreach (Role::PANITIA_ALL as $panitia){
+                    User::create([
+                        'id' => ++$id_panitia,
+                        'nama' => $faker->unique()->name,
+                        'password' => bcrypt('secret'),
+                        'role' => $role,
+                        'helper' => $panitia
+                    ]);
+                }
+            }
+            else{
+                User::create([
+                    'id' => $id_others,
+                    'nama' => $faker->unique()->name,
+                    'password' => bcrypt('secret'),
+                    'role' => $role,
+                ]);
+                $id_others += 10;
+            }
+        }
     }
 }
