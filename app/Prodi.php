d5581066957a8f887269659eb84ab4c52a32faa3
Rafy Aulia Akbar <rafy683@gmail.com>
2018-01-11 16:14:11 +0700

    model Prodi

--- app/Prodi.php
+++ app/Prodi.php
@@ -6,5 +6,39 @@ use Illuminate\Database\Eloquent\Model;
 
 class Prodi extends Model
 {
-    //
+    protected $table = 'prodi';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'nama', 'jurusan_id'
+    ];
+
+    /**
+     * Mendapatkan data mahasiswa dengan prodi ini
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function mahasiswa()
+    {
+        return $this->hasMany('Voting\Mahasiswa', 'prodi_id');
+    }
+
+    /**
+     * Mendapatkan data jurusan dari prodi ini
+     * @return Model|null|static
+     */
+    public function jurusan()
+    {
+        return $this->belongsTo('Voting\Jurusan', 'jurusan_id')->first();
+    }
+
+    /**
+     * Mendapatkan data prodi dari nama
+     * @param $nama
+     * @return mixed
+     */
+    public static function getByName($nama)
+    {
+        return Prodi::where('nama', $nama)->first();
+    }
 }
