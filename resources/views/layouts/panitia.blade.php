473173e7146249bc9212d2a22df9b318efa361ce
m1ku100 <ilham.puji100@gmail.com>
2018-01-12 17:33:55 +0700

    minor change global blade

--- resources/views/layouts/panitia.blade.php
+++ resources/views/layouts/panitia.blade.php
@@ -1,16 +1,16 @@
-<li @if (Route::currentRouteName() == '') class="active" @endif>
-    <a href="{{route('')}}">
+<li @if (Route::currentRouteName() == 'panitia.dashboard') class="active" @endif>
+    <a href="{{route('panitia.dashboard')}}">
         <i class="fa fa-home"></i> Dashboard
     </a>
 </li>
-<li @if (Route::currentRouteName() == '') class="active" @endif>
-    <a href="{{route('')}}">
-        <i class="fa fa-th-list"></i> Data Paslon
-    </a>
-</li>
+{{--<li @if (Route::currentRouteName() == '') class="active" @endif>--}}
+    {{--<a href="{{route('')}}">--}}
+        {{--<i class="fa fa-th-list"></i> Data Paslon--}}
+    {{--</a>--}}
+{{--</li>--}}
 @if (\Illuminate\Support\Facades\Auth::user()->helper=='kps')
-    <li @if (Route::currentRouteName() == '') class="active" @endif>
-        <a href="{{route('')}}">
+    <li @if (Route::currentRouteName() == 'panitia.resepsionis') class="active" @endif>
+        <a href="{{route('panitia.resepsionis')}}">
             <i class="fa fa-th-list"></i> Resepsionis
         </a>
     </li>
