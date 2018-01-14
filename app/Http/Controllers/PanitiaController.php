ac966556f93a16c7115f338ed46fffeef8a534c2
m1ku100 <ilham.puji100@gmail.com>
2018-01-14 16:50:25 +0700

    upload image untuk hmj

--- app/Http/Controllers/PanitiaController.php
+++ app/Http/Controllers/PanitiaController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\CalonHMJ;
 use App\Mahasiswa;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 
 
 class PanitiaController extends Controller
@@ -29,9 +31,65 @@ class PanitiaController extends Controller
         return view('admin.panitia.paslon');
     }
 
-    public function paslonform()
+    public function formhmj()
     {
         return view('admin.panitia.form');
+    }
+
+    public function hmjsave(Request $request)
+    {
+        $request->validate([
+            'ketua_id' => 'required',
+            'wakil_id' => 'required',
+            'visi' => 'required',
+            'misi' => 'required',
+            'nomor'=>'required',
+            'dir'=>'required',
+        ]);
+
+        $id = CalonHMJ::count();
+        $idnow = $id + 1;
+
+
+        if ($request->hasFile('dir')) {
+
+            $fillnames2 = $request->dir->getClientOriginalName() . '' . str_random(4);
+            $filename = 'upload/photo/hmj/'
+                . str_slug($fillnames2, '-') . '.' . $request->dir->getClientOriginalExtension();
+            $request->dir->storeAs('public', $filename);
+            $berkas = new CalonHMJ();
+            $berkas->dir = $filename;
+            $berkas->ketua_id = $request->ketua_id;
+            $berkas->wakil_id = $request->wakil_id;
+            $berkas->nomor = $request->nomor;
+            $berkas->visi = $request->visi;
+            $berkas->misi = $request->misi;
+            $berkas->save();
+            $dir = $fillnames2;
+
+        }
+
+        return redirect('panitia/paslon')->with('message','Paslon Berhasil Ditambahkan');
+    }
+
+    public function formdpm()
+    {
+        return view('admin.panitia.formdpm');
+    }
+
+    public function dpmsave(Request $request)
+    {
+
+    }
+
+    public function formbem()
+    {
+        return view('admin.panitia.formbem');
+    }
+
+    public function bemsave(Request $request)
+    {
+
     }
 
     public function resepsionis()
