f072d66eeed279c127be7a9577e18bd4a0cd1c97
Rafy Aulia Akbar <rafy683@gmail.com>
2018-01-15 00:24:16 +0700

    penambahan method getWaktuMulai dan getWaktuSelesai pada Pengaturan

--- app/Pengaturan.php
+++ app/Pengaturan.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Pengaturan extends Model
 {
@@ -17,4 +18,22 @@ class Pengaturan extends Model
     protected $fillable = [
         'key', 'value'
     ];
+
+    /**
+     * mendapatkan waktu mulai voting
+     * @return static
+     */
+    public static function getWaktuMulai()
+    {
+        return Carbon::parse(Pengaturan::find('mulai')->value);
+    }
+
+    /**
+     * mendapatkan waktu selesai voting
+     * @return static
+     */
+    public static function getWaktuSelesai()
+    {
+        return Carbon::parse(Pengaturan::find('selesai')->value);
+    }
 }
