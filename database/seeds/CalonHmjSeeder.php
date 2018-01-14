b508d0d361f81c59f539e71331295c269a1b464d
Rafy Aulia Akbar <rafy683@gmail.com>
2018-01-14 15:47:54 +0700

    update calonhmjseeder

--- database/seeds/CalonHmjSeeder.php
+++ database/seeds/CalonHmjSeeder.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\CalonHMJ;
+use App\CalonBEM;
+use App\CalonDPM;
+use App\Jurusan;
+use App\Mahasiswa;
 
 class CalonHmjSeeder extends Seeder
 {
@@ -11,6 +16,27 @@ class CalonHmjSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $faker = \Faker\Factory::create();
+        foreach (Jurusan::all() as $jurusan){
+            $jumlahCalon = rand(2, 4);
+            $mahasiswa = $jurusan->getMahasiswa()
+                ->whereIn('status', [Mahasiswa::AKTIF])
+                ->whereNotIn('id', CalonBEM::getAllIdCalon())
+                ->whereNotIn('id', CalonDPM::getAllIdAnggota())
+                ->get();
+            $acakMahasiswa = array_rand($mahasiswa->toArray(), $jumlahCalon * 2);
+            $counter = 0;
+            for ($c = 0; $c < $jumlahCalon; $c++){
+                CalonHMJ::create([
+                    'ketua_id' => $mahasiswa->toArray()[$acakMahasiswa[$counter++]]['id'],
+                    'wakil_id' => $mahasiswa->toArray()[$acakMahasiswa[$counter++]]['id'],
+                    'dir' => 'http://www.voa-islam.com/photos5/GXaPe9UdAH.png',
+                    'visi' => $faker->unique()->text,
+                    'misi' => $faker->unique()->text(600),
+                    'nomor' => $c + 1
+                ]);
+
+            }
+        }
     }
 }
