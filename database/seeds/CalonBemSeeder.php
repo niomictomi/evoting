c05d06964e810d0df22e43dbc585b9ee9a02a674
Rafy Aulia Akbar <rafy683@gmail.com>
2018-01-14 14:25:41 +0700

    penambahan file beberapa seeder

---
+++ database/seeds/CalonBemSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CalonBemSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
