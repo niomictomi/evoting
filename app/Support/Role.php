534396281be21c6a294d919ec438d14c29446b33
Rafy Aulia Akbar <rafy683@gmail.com>
2018-01-14 21:08:07 +0700

    penambahan method check pada class Role

--- app/Support/Role.php
+++ app/Support/Role.php
@@ -24,12 +24,12 @@ class Role
     const PANITIA = 'panitia';
 
     const ALL = [
-        'panitia',
-        'ketua kpu',
-        'dosen',
-        'wd3',
-        'admin',
-        'root'
+        Role::PANITIA,
+        Role::KETUA_KPU,
+        Role::DOSEN,
+        Role::WD3,
+        Role::ADMIN,
+        Role::ROOT
     ];
 
     const PANITIA_KPU = 'kpu';
@@ -39,8 +39,29 @@ class Role
     const PANITIA_KPS = 'kps';
 
     const PANITIA_ALL = [
-        'kpu',
-        'bawaslu',
-        'kps'
+        Role::KETUA_KPU,
+        Role::PANITIA_BAWASLU,
+        Role::PANITIA_KPS
     ];
+
+    /**
+     * mengecek apakah role ada atau tidak
+     * @param $role_name
+     * @param null $delimitter
+     * @return bool
+     */
+    public static function check($role_name, $delimitter = null)
+    {
+        if (is_null($delimitter)){
+            if (in_array($role_name, Role::ALL))
+                return true;
+        }
+        else{
+            if (in_array(explode($delimitter, $role_name)[0], Role::ALL))
+                if (in_array(explode($delimitter, $role_name)[0], Role::PANITIA_ALL))
+                    return true;
+        }
+
+        return false;
+    }
 }
