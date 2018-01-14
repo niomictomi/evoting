421e4b02a3e427fa0bbc5596e461ad38476f4804
Rafy Aulia Akbar <rafy683@gmail.com>
2018-01-14 17:50:13 +0700

    minor update

--- resources/views/admin/admin/panitia.blade.php
+++ resources/views/admin/admin/panitia.blade.php
@@ -11,7 +11,7 @@
                 <h3 class="title">Daftar panitia</h3>
             </div>
             <div class="header-block pull-right">
-                <a href="#tambah" class="btn btn-primary btn-sm rounded pull-right" data-toggle="modal">Tambah Panitia</a>
+                <button data-target="#tambah" class="btn btn-primary btn-sm rounded pull-right" data-toggle="modal">Tambah Panitia</button>
                 <div class="modal fade" id="tambah" tabindex="-1" role="dialog"
                      aria-hidden="true">
                     <div class="modal-dialog" role="document">
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -10,3 +10,27 @@
         Panitia
     </a>
 </li>
+<li @if(Route::currentRouteName() == 'admin.paniti') class="active" @endif>
+    <a href="{{ route('admin.panitia') }}">
+        <i class="fa fa-thermometer-half"></i>&nbsp;&nbsp;
+        Voting HMJ
+    </a>
+</li>
+<li @if(Route::currentRouteName() == 'admin.paniti') class="active" @endif>
+    <a href="{{ route('admin.panitia') }}">
+        <i class="fa fa-thermometer-three-quarters"></i>&nbsp;&nbsp;
+        Voting DPM
+    </a>
+</li>
+<li @if(Route::currentRouteName() == 'admin.panita') class="active" @endif>
+    <a href="{{ route('admin.panitia') }}">
+        <i class="fa fa-thermometer-full"></i>&nbsp;&nbsp;
+        Voting BEM
+    </a>
+</li>
+<li @if(Route::currentRouteName() == 'admin.paniia') class="active" @endif>
+    <a href="{{ route('admin.panitia') }}">
+        <i class="fa fa-gears"></i>
+        Pengaturan Voting
+    </a>
+</li>
