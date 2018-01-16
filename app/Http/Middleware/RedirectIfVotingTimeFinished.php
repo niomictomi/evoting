074933d3c460081e1446b7ef2dcdae10ed129db4
BagasMuharom <bagashidayat45@gmail.com>
2018-01-16 19:21:56 +0700

    Membuat middleware untuk pengecekan waktu voting telah selesai

---
+++ app/Http/Middleware/RedirectIfVotingTimeFinished.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use App\Pengaturan;
+
+class CekTanggal
+{
+    
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if(Pengaturan::isVotingTelahBerlangsung())
+            return abort(404);
+
+        return $next($request);
+    }
+
+}
