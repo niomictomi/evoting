a9fa4562ecf74d33b6a56a863c95fa1a63d03284
BagasMuharom <bagashidayat45@gmail.com>
2018-01-16 19:46:50 +0700

    Menambah menu kelola admin untuk root

--- resources/views/layouts/root.blade.php
+++ resources/views/layouts/root.blade.php
@@ -10,6 +10,12 @@
     </a>
 </li>
 
+<li @if (Route::currentRouteName() == 'root.admin') class="active" @endif>
+    <a href="{{ route('root.admin') }}">
+        <i class="fa fa-list"></i> Kelola Admin
+    </a>
+</li>
+
 <li @if (Route::currentRouteName() == 'root.reset') class="active" @endif>
     <a href="{{ route('root.reset') }}">
         <i class="fa fa-refresh"></i> Reset
