dac01092ad92a326921ef68533ecfe45f673346d
BagasMuharom <bagashidayat45@gmail.com>
2018-01-16 19:22:20 +0700

    Menambah method untuk kelola admin

--- app/Http/Controllers/Page/RootController.php
+++ app/Http/Controllers/Page/RootController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Page;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Jurusan;
+use App\Support\Role;
+use App\User;
 
 class RootController extends Controller
 {
@@ -45,6 +47,19 @@ class RootController extends Controller
     public function reset()
     {
         return view('admin.root.reset');
+    }
+
+    /**
+     * Menampilkan halaman untuk mengelola admin
+     *
+     * @return Illuminate\View\View
+     */
+    public function admin()
+    {
+        $daftarAdmin = User::where('role', Role::ADMIN)->get();
+        return view('admin.root.admin', [
+            'daftarAdmin' => $daftarAdmin
+        ]);
     }
 
 }
