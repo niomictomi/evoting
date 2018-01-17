f0099ef5509714d99382bffb5e3cab3495bc134d
m1ku100 <ilham.puji100@gmail.com>
2018-01-17 18:27:28 +0700

    UI buka suara untuk dosen dan Wd3 fix

--- app/Http/Controllers/Wd3dosenController.php
+++ app/Http/Controllers/Wd3dosenController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class Wd3dosenController extends Controller
 {
@@ -25,5 +28,26 @@ class Wd3dosenController extends Controller
     public function buka()
     {
       return view('admin.wd3dosen.bukasuara');
+    }
+
+    public function save( Request $request)
+    {
+        $this->validate($request, [
+            'helper' => 'required',
+        ]);
+        $user = User::find($request->id);
+        if(!Hash::check($request->helper, Auth::user()->password)) {
+            return back()->with([
+                'error' => 'Kata sandi anda salah !'
+            ]);
+        }else{
+            $user->update([
+                'helper' =>$request->helper,
+            ]);
+            return back()->with([
+                'message' => 'Password telah disimpan'
+            ]);
+        }
+
     }
 }
--- resources/views/admin/wd3dosen/bukasuara.blade.php
+++ resources/views/admin/wd3dosen/bukasuara.blade.php
@@ -3,97 +3,6 @@
 @section('content')
     <div class="row sameheight-container">
         @if (\Illuminate\Support\Facades\Auth::user()->role=='wd3')
-        <div class="col">
-            <div class="card">
-                <div class="card-block">
-                    <div class="title-block">
-                        <h4 class="title">Wakil Dekan 3</h4>
-                    </div>
-
-                    <form action="{{ route('admin.pengaturan') }}" method="post">
-
-                        {{ csrf_field() }}
-
-                        <div class="form-group row">
-                            <label class="col-md-4" for="id">Password</label>
-                            <div class="col-md-8">
-                                <input type="text" name="helper" class="form-control"/>
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label class="col-md-4"></label>
-                            <div class="col-md-8">
-                                <button type="submit" class="btn btn-primary">Ubah</button>
-                            </div>
-                        </div>
-
-                    </form>
-                </div>
-            </div>
-        </div>
-
-        <div class="col">
-            <div class="card">
-                <div class="card-block">
-                    <div class="title-block">
-                        <h4 class="title">Dosen</h4>
-                    </div>
-
-                    <form action="{{ route('admin.ubah.password') }}" method="post">
-
-                        {{ csrf_field() }}
-
-                        <div class="form-group row">
-                            <label class="col-md-4" for="id">Password</label>
-                            <div class="col-md-8">
-                                <input type="password" class="form-control" name="passlama" disabled=""/>
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label class="col-md-4"></label>
-                            <div class="col-md-8">
-                                <button type="submit" class="btn btn-primary" disabled="">Ubah</button>
-                            </div>
-                        </div>
-
-                    </form>
-                </div>
-            </div>
-        </div>
-
-        <div class="col">
-            <div class="card">
-                <div class="card-block">
-                    <div class="title-block">
-                        <h4 class="title">Ketua KPU</h4>
-                    </div>
-
-                    <form action="{{ route('admin.ubah.password') }}" method="post">
-
-                        {{ csrf_field() }}
-
-
-                        <div class="form-group row">
-                            <label class="col-md-4" for="id">Password</label>
-                            <div class="col-md-8">
-                                <input type="password" class="form-control" name="passbaru_confirmation" disabled/>
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label class="col-md-4"></label>
-                            <div class="col-md-8">
-                                <button type="submit" class="btn btn-primary" disabled>Ubah</button>
-                            </div>
-                        </div>
-
-                    </form>
-                </div>
-            </div>
-        </div>
-            @elseif(\Illuminate\Support\Facades\Auth::user()->role=='dosen')
             <div class="col">
                 <div class="card">
                     <div class="card-block">
@@ -101,21 +10,31 @@
                             <h4 class="title">Wakil Dekan 3</h4>
                         </div>
 
-                        <form action="{{ route('admin.pengaturan') }}" method="post">
+                        <form action="{{ url('dosen/buka/'.\Illuminate\Support\Facades\Auth::user()->id.'/save') }}"
+                              method="post">
 
                             {{ csrf_field() }}
 
-                            <div class="form-group row">
-                                <label class="col-md-4" for="id">Password</label>
-                                <div class="col-md-8">
-                                    <input type="text" name="nama" class="form-control" disabled/>
+                            @if(\Illuminate\Support\Facades\Auth::user()->helper ==null)
+                                <div class="form-group row">
+                                    <label class="col-md-4" for="id">Password</label>
+                                    <div class="col-md-8">
+                                        <input type="password" class="form-control" name="helper"/>
+                                    </div>
                                 </div>
-                            </div>
-
+                            @else
+                                <div class="alert alert-info">
+                                    Password Telah Disimpan
+                                </div>
+                            @endif
                             <div class="form-group row">
                                 <label class="col-md-4"></label>
                                 <div class="col-md-8">
-                                    <button type="submit" class="btn btn-primary" disabled>Ubah</button>
+                                    @if(\Illuminate\Support\Facades\Auth::user()->helper ==null)
+                                        <button type="submit" class="btn btn-primary">Submit</button>
+                                    @else
+                                        <button type="submit" class="btn btn-primary" disabled="">Saved</button>
+                                    @endif
                                 </div>
                             </div>
 
@@ -131,21 +50,126 @@
                             <h4 class="title">Dosen</h4>
                         </div>
 
-                        <form action="{{ route('admin.ubah.password') }}" method="post">
+                        <form action="{{ url('dosen/buka/'.\Illuminate\Support\Facades\Auth::user()->id.'/save') }}"
+                              method="post">
 
                             {{ csrf_field() }}
 
                             <div class="form-group row">
                                 <label class="col-md-4" for="id">Password</label>
                                 <div class="col-md-8">
-                                    <input type="password" class="form-control" name="helper" />
+                                    <input type="password" class="form-control" name="passlama" disabled=""/>
                                 </div>
                             </div>
 
                             <div class="form-group row">
                                 <label class="col-md-4"></label>
                                 <div class="col-md-8">
-                                    <button type="submit" class="btn btn-primary" >Ubah</button>
+                                    <button type="submit" class="btn btn-primary" disabled="">Ubah</button>
+                                </div>
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="card">
+                    <div class="card-block">
+                        <div class="title-block">
+                            <h4 class="title">Ketua KPU</h4>
+                        </div>
+
+                        <form action="{{ route('admin.ubah.password') }}" method="post">
+
+                            {{ csrf_field() }}
+
+
+                            <div class="form-group row">
+                                <label class="col-md-4" for="id">Password</label>
+                                <div class="col-md-8">
+                                    <input type="password" class="form-control" name="passbaru_confirmation" disabled/>
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label class="col-md-4"></label>
+                                <div class="col-md-8">
+                                    <button type="submit" class="btn btn-primary" disabled>Ubah</button>
+                                </div>
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+
+        @elseif(\Illuminate\Support\Facades\Auth::user()->role=='dosen')
+
+            <div class="col">
+                    <div class="card">
+                        <div class="card-block">
+                            <div class="title-block">
+                                <h4 class="title">Wakil Dekan 3</h4>
+                            </div>
+
+                            <form action="{{ route('admin.pengaturan') }}" method="post">
+
+                                {{ csrf_field() }}
+
+                                <div class="form-group row">
+                                    <label class="col-md-4" for="id">Password</label>
+                                    <div class="col-md-8">
+                                        <input type="text" name="nama" class="form-control" disabled/>
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label class="col-md-4"></label>
+                                    <div class="col-md-8">
+                                        <button type="submit" class="btn btn-primary" disabled>Ubah</button>
+                                    </div>
+                                </div>
+
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
+            <div class="col">
+                <div class="card">
+                    <div class="card-block">
+                        <div class="title-block">
+                            <h4 class="title">Dosen</h4>
+                        </div>
+
+                        <form action="{{ url('dosen/buka/'.\Illuminate\Support\Facades\Auth::user()->id.'/save') }}"
+                              method="post">
+
+                            {{ csrf_field() }}
+
+                            @if(\Illuminate\Support\Facades\Auth::user()->helper ==null)
+                                <div class="form-group row">
+                                    <label class="col-md-4" for="id">Password</label>
+                                    <div class="col-md-8">
+                                        <input type="password" class="form-control" name="helper"/>
+                                    </div>
+                                </div>
+                            @else
+                                <div class="alert alert-info">
+                                    Password Telah Disimpan
+                                </div>
+                            @endif
+                            <div class="form-group row">
+                                <label class="col-md-4"></label>
+                                <div class="col-md-8">
+                                    @if(\Illuminate\Support\Facades\Auth::user()->helper ==null)
+                                        <button type="submit" class="btn btn-primary">Submit</button>
+                                    @else
+                                        <button type="submit" class="btn btn-primary" disabled="">Saved</button>
+                                    @endif
                                 </div>
                             </div>
 
@@ -187,3 +211,24 @@
         @endif
     </div>
 @endsection
+@push('js')
+    @if(session()->has('message'))
+        <script>
+            swal({
+                icon: "success",
+                title: "{{ session()->get('message') }}"
+            });
+
+        </script>
+
+    @endif
+    @if (session()->has('error'))
+        <script>
+            swal({
+                icon: "error",
+                title: "{{ session()->get('error') }}"
+            });
+
+        </script>
+    @endif
+@endpush
