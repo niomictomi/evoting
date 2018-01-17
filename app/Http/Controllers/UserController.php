e854a72852ebffcb72d44004db35ad6c4d32c5c7
BagasMuharom <bagashidayat45@gmail.com>
2018-01-17 10:32:34 +0700

    Kelupaan UserController belum di push

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('bukanmhs');
+    }
+    
+    /**
+     * Mengubah profil user
+     * route: admin.pengaturan
+     *
+     * @param Request $request
+     * @return Illuminate\Http\RedirectResponse
+     */
+    public function pengaturan(Request $request)
+    {
+        $arrValidation = [
+            'nama' => 'required'
+        ];
+
+        if(Auth::user()->isRoot())
+            $arrValidation['id'] = 'required|numeric|unique:users,id,'.Auth::user()->id;
+
+        $this->validate($request, $arrValidation);
+
+        Auth::user()->update([
+            'nama' => $request->nama
+        ]);
+
+        if(Auth::user()->isRoot()) {
+            Auth::user()->update([
+                'id' => $request->id
+            ]);
+        }
+
+        return back()->with([
+            'success' => 'Berhasil mengubah profil !'
+        ]);
+    }
+
+    /**
+     * Mengubah kata sandi user
+     * route: admin.ubah.password
+     *
+     * @param Request $request
+     * @return Illuminate\Http\RedirectResponse
+     */
+    public function ubahPassword(Request $request)
+    {  
+        if(!Hash::check($request->passlama, Auth::user()->password)) {
+            return back()->with([
+                'error' => 'Kata sandi lama anda salah !'
+            ]);
+        }
+        else {
+            if($request->passbaru !== $request->passbaru_confirmation) {
+                return back()->with([
+                    'error' => 'Kata sandi yang anda masukkan tidak sama !'
+                ]); 
+            }
+            else {
+                Auth::user()->update([
+                    'password' => bcrypt($request->passbaru)
+                ]);
+
+                return back()->with([
+                    'success' => 'Berhasil mengubah kata sandi 1 !'
+                ]);
+            }
+        }
+    }
+
+}
