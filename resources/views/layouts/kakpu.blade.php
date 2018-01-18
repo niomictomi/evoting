e6c2f7a52a15c648f0ad8d20e1302c382965075b
Rafy Aulia Akbar <rafy683@gmail.com>
2018-01-19 05:25:35 +0700

    penambahan beberapa menu untuk ketua kpu

--- resources/views/layouts/kakpu.blade.php
+++ resources/views/layouts/kakpu.blade.php
@@ -1,8 +1,36 @@
 <li @if (Route::currentRouteName() == 'kakpu.dashboard') class="active" @endif>
     <a href="{{route('kakpu.dashboard')}}">
-        <i class="fa fa-home"></i> Dashboard </a>
+        <i class="fa fa-home"></i>
+        Dashboard
+    </a>
+</li>
+<li @if(Route::currentRouteName() == 'admin.voting.hmj') class="active" @endif>
+    <a href="{{ route('admin.voting.hmj', ['jurusan' => \App\Jurusan::all()->first()->nama, 'tipe' => 'Memiliki hak suara']) }}">
+        <i class="fa fa-thermometer-half"></i>&nbsp;&nbsp;
+        Voting HMJ
+    </a>
+</li>
+<li @if(Route::currentRouteName() == 'admin.voting.dpm') class="active" @endif>
+    <a href="{{ route('admin.voting.dpm', ['jurusan' => \App\Jurusan::all()->first()->nama, 'tipe' => 'Memiliki hak suara']) }}}">
+        <i class="fa fa-thermometer-three-quarters"></i>&nbsp;&nbsp;
+        Voting DPM
+    </a>
+</li>
+<li @if(Route::currentRouteName() == 'admin.voting.bem') class="active" @endif>
+    <a href="{{ route('admin.voting.bem', ['tipe' => 'Memiliki hak suara']) }}">
+        <i class="fa fa-thermometer-full"></i>&nbsp;&nbsp;
+        Voting BEM
+    </a>
 </li>
 <li @if (Route::currentRouteName() == 'kakpu.buka') class="active" @endif>
     <a href="{{route('kakpu.buka')}}">
-        <i class="fa fa-key"></i> Buka Kotak Suara </a>
+        <i class="fa fa-key"></i>
+        Buka Kotak Suara
+    </a>
+</li>
+<li @if(Route::currentRouteName() == 'admin.voting.pengaturan') class="active" @endif>
+    <a href="{{ route('admin.voting.pengaturan') }}">
+        <i class="fa fa-gears"></i>
+        Pengaturan Voting
+    </a>
 </li>
