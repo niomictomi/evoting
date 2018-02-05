9970de66e28fb3a2f26d9f2224ac7f59372cb6b9
Rafy Aulia Akbar <rafy683@gmail.com>
2018-02-05 20:56:08 +0700

    update AdminController

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -98,54 +98,55 @@ class AdminController extends Controller
             'role' => 'required'
         ]);
 
-        if ((count(explode(';', $request->role)) > 1) ? Role::check($request->role, ';') : Role::check($request->role)) {
-            $role = explode(';', $request->role);
-            $user = User::find($request->id_lama);
-            if (count($role) > 1) {
-                if (is_null($request->password)){
+        if ($request->role != Role::WD3 && $request->role != Role::DOSEN && $request->role != Role::ROOT){
+            if ((count(explode(';', $request->role)) > 1) ? Role::check($request->role, ';') : Role::check($request->role)) {
+                $role = explode(';', $request->role);
+                $user = User::find($request->id_lama);
+                if (count($role) > 1) {
+                    if (is_null($request->password)){
+                        $user->update([
+                            'nama' => $request->nama,
+                            'id' => $request->id,
+                            'role' => $role[0],
+                            'helper' => $role[1]
+                        ]);
+
+                        return back()->with('message', 'Berhasil memperbarui data '.$user->nama.'.');
+                    }
                     $user->update([
                         'nama' => $request->nama,
                         'id' => $request->id,
                         'role' => $role[0],
-                        'helper' => $role[1]
+                        'helper' => $role[1],
+                        'password' => bcrypt($request->password)
                     ]);
 
-                    return back()->with('message', 'Berhasil memperbarui data '.$user->nama.'.');
-                }
-                $user->update([
-                    'nama' => $request->nama,
-                    'id' => $request->id,
-                    'role' => $role[0],
-                    'helper' => $role[1],
-                    'password' => bcrypt($request->password)
-                ]);
+                    return back()->with('message', 'Berhasil memperbarui data '.$user->nama.' beserta password.');
+                } else {
+                    if (is_null($request->password)){
+                        $user->update([
+                            'nama' => $request->nama,
+                            'id' => $request->id,
+                            'role' => $role[0],
+                            'helper' => $role[1]
+                        ]);
 
-                return back()->with('message', 'Berhasil memperbarui data '.$user->nama.' beserta password.');
-            } else {
-                if (is_null($request->password)){
+                        return back()->with('message', 'Berhasil memperbarui data '.$user->nama.'.');
+                    }
                     $user->update([
                         'nama' => $request->nama,
                         'id' => $request->id,
                         'role' => $role[0],
-                        'helper' => $role[1]
+                        'helper' => $role[1],
+                        'password' => bcrypt($request->password)
                     ]);
 
-                    return back()->with('message', 'Berhasil memperbarui data '.$user->nama.'.');
+                    return back()->with('message', 'Berhasil memperbarui data '.$user->nama.' beserta password.');
                 }
-                $user->update([
-                    'nama' => $request->nama,
-                    'id' => $request->id,
-                    'role' => $role[0],
-                    'helper' => $role[1],
-                    'password' => bcrypt($request->password)
-                ]);
-
-                return back()->with('message', 'Berhasil memperbarui data '.$user->nama.' beserta password.');
             }
         }
 
-        $request->role = str_replace(';', ' - ', $request->role);
-        return back()->withErrors(['Hak akses "' . strtoupper($request->role) . '" tidak tersedia!']);
+        return back()->withErrors(['Anda tidak bisa mengedit '.strtoupper($request->role)]);
     }
 
     public function hapusPanitia(Request $request)
