fe032cf1feeb039712af3849b376b6d95d0cd563
m1ku100 <ilham.puji100@gmail.com>
2018-02-14 22:33:40 +0700

    lock pengaturan ketika voting selesai

--- resources/views/admin/public/pengaturanvoting.blade.php
+++ resources/views/admin/public/pengaturanvoting.blade.php
@@ -28,27 +28,33 @@
             </div>
         </div>
         <div class="card-block">
-            <form action="{{ route('admin.voting.pengaturan.update') }}" method="post">
-                {{ csrf_field() }}
-                {{ method_field('patch') }}
-                <div class="form-group">
-                    <label class="control-label">Mulai</label>
-                    <div class="card-block" style="background-color: #e9ecef">
-                        <input class="form-control" type="hidden" id="mulai" name="mulai" value="{{ $mulai }}">
+            @if(!\App\Pengaturan::isVotingTelahBerlangsung())
+                <form action="{{ route('admin.voting.pengaturan.update') }}" method="post">
+                    {{ csrf_field() }}
+                    {{ method_field('patch') }}
+                    <div class="form-group">
+                        <label class="control-label">Mulai</label>
+                        <div class="card-block" style="background-color: #e9ecef">
+                            <input class="form-control" type="hidden" id="mulai" name="mulai" value="{{ $mulai }}">
+                        </div>
                     </div>
-                </div>
-                <div class="form-group">
-                    <label class="control-label">Selesai</label>
-                    <div class="card-block" style="background-color: #e9ecef">
-                        <input class="form-control" type="hidden" id="selesai" name="selesai" value="{{ $selesai }}">
+                    <div class="form-group">
+                        <label class="control-label">Selesai</label>
+                        <div class="card-block" style="background-color: #e9ecef">
+                            <input class="form-control" type="hidden" id="selesai" name="selesai" value="{{ $selesai }}">
+                        </div>
                     </div>
+                    <div class="form-group">
+                        <label class="control-label">Panjang password untuk mahasiswa</label>
+                        <input class="form-control" name="panjang_password" value="{{ $max_password }}" type="number" min="5">
+                    </div>
+                    <input type="submit" class="btn btn-success" value="Simpan" style="color: white">
+                </form>
+            @else
+                <div class="alert alert-warning">
+                    Voting telah selesai!
                 </div>
-                <div class="form-group">
-                    <label class="control-label">Panjang password untuk mahasiswa</label>
-                    <input class="form-control" name="panjang_password" value="{{ $max_password }}" type="number" min="5">
-                </div>
-                <input type="submit" class="btn btn-success" value="Simpan" style="color: white">
-            </form>
+            @endif
         </div>
     </div>
 @endsection
