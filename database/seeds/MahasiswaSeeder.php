7a1f81fe7966be8ece383361fbe56916ba480594
Unknown <rafy683@gmail.com>
2018-09-29 15:21:07 +0700

    update mahasiswaseeder

--- database/seeds/MahasiswaSeeder.php
+++ database/seeds/MahasiswaSeeder.php
@@ -12,8 +12,22 @@ class MahasiswaSeeder extends Seeder
      */
     public function run()
     {
-        factory(Mahasiswa::class, 3000)->create()->each(function ($u){
-            $u->make();
-        });
+        $faker = \Faker\Factory::create('ID_id');
+        $prodi = \App\Prodi::all();
+
+        for ($c = 0; $c < 3000; $c++){
+            $status = Mahasiswa::STATUS[array_rand(Mahasiswa::STATUS, 1)];
+            if ($status == Mahasiswa::NONAKTIF || $status == Mahasiswa::CUTI)
+                $status = Mahasiswa::STATUS[array_rand(Mahasiswa::STATUS, 1)];
+            if ($status == Mahasiswa::NONAKTIF || $status == Mahasiswa::CUTI)
+                $status = Mahasiswa::STATUS[array_rand(Mahasiswa::STATUS, 1)];
+
+            Mahasiswa::create([
+                'id' => rand(11, 17).$faker->unique()->numerify('#########'),
+                'prodi_id' => $prodi->random()->id,
+                'nama' => $faker->unique()->name(),
+                'status' => $status
+            ]);
+        }
     }
 }
